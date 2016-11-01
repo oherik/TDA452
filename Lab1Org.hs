18c8224a4add084be043955a7b9a415e94e0ff58
michelle-tl <miss_amtl@live.com>
2016-11-01 09:50:55 +0100

    draft of lab1

--- Lab1Org.hs
+++ Lab1Org.hs
@@ -20,18 +20,35 @@ power2 n k | odd k = n * power2 n (k-1)
 
 -- Part 4
 -- A
-We would like to test with integers since the function should not be defined for floats. We want to make sure that k is greater than 0, if not it will return an error.
+-- We would like to test with integers since the function should not be defined for floats. We will not test when k is less than 0, since those are not valid numbers.
+--
+-- The base cases we have chosen is k=0 and n=0. For negative n we want to check it with odd and even k's, to make sure that the result is either negative or positive. We also want to test positive n's with odd and even k's.
+--
+-- Cases:
+-- n=2 k=0
+-- n=0 k=2
+-- n=-2 k=2
+-- n=-2 k=3
+-- n=2 k=2
+-- n=2 k=3
 
-The base cases we have chosen is k=0 and n=0. For negative n we want to check it with odd and even k's, to make sure that the result is either negative or positive. We also want to test positive n's with odd and even k's.
+-- B
+power :: Integer -> Integer -> Integer
+power n k | k < 0 = error "power: negative argument"
+power n 0 = 1
+power n k = n * power n (k-1)
 
-Cases:
-n=2 k=-2
-n=2 k=0
-n=0 k=2
-n=-2 k=2
-n=-2 k=3
-n=2 k=2
-n=2 k=3
+prop_powers :: Integer -> Integer -> Bool
+prop_powers n k = (power n k == power1 n k) &&
+                (power1 n k == power2 n k) &&
+                (power2 n k == n ^ k)
 
-pow_test :: Integer -> Integer -> Boolean
-pow_test n k = pow_
+-- C
+power_tests = prop_powers 2 0 &&
+              prop_powers 0 2 &&
+              prop_powers (-2) 2 &&
+              prop_powers (-2) 3 &&
+              prop_powers 2 2 &&
+              prop_powers 2 3
+
+-- D
