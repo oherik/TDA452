3ba6762706555cfe8fc679203ce35cabc62d97bf
Erik Öhrn <erik.ohrn@outlook.com>
2016-11-09 19:21:41 +0100

    Made all line lengths <78

--- Lab2b/BlackJack.hs
+++ Lab2b/BlackJack.hs
@@ -85,7 +85,9 @@ module BlackJack where
 
   -- Add all cards for the given suit to a list
   createFullSuit :: Suit -> [Card]
-  createFullSuit suit = [(Card Ace suit)] ++ [(Card (Numeric a) suit) | a <- [2..10]] ++ [(Card b suit) | b <- [Jack, Queen, King]]
+  createFullSuit suit = [(Card Ace suit)] ++
+                        [(Card (Numeric a) suit) | a <- [2..10]] ++
+                        [(Card b suit) | b <- [Jack, Queen, King]]
 
   -- Given a list, return a hand with the cards of the list
   combineCards :: [Card] -> Hand
@@ -93,11 +95,15 @@ module BlackJack where
   combineCards (card:xs) = Add card (combineCards xs)
 
   fullDeck :: Hand
-  fullDeck = combineCards (createFullSuit Hearts ++ createFullSuit Spades ++ createFullSuit Diamonds ++ createFullSuit Clubs)
+  fullDeck = combineCards (createFullSuit Hearts ++
+                          createFullSuit Spades ++
+                          createFullSuit Diamonds ++
+                          createFullSuit Clubs)
 
   draw :: Hand -> Hand -> (Hand,Hand)
   draw Empty _ = error "draw: The deck is empty"
-  draw (Add topDeckCard restOfDeck) hand = (restOfDeck, (Add topDeckCard hand))
+  draw (Add topDeckCard restOfDeck) hand =
+          (restOfDeck, (Add topDeckCard hand))
 
   -- Plays the bank, starting with an empty hand
   playBank :: Hand -> Hand
@@ -105,7 +111,8 @@ module BlackJack where
 
   -- The bank draws from the deck until its hand value is 16 or above
   playBank' :: Hand -> Hand -> Hand
-  playBank' deck bankHand | value bankHand < 16 = playBank' deck1' bankHand1'
+  playBank' deck bankHand | value bankHand < 16 =
+                                    playBank' deck1' bankHand1'
                        | otherwise = bankHand
                        where (deck1',bankHand1') = draw deck bankHand
 
@@ -128,10 +135,10 @@ module BlackJack where
   -- need the addReverse function, instead of the previously created (<+)
   removeCard' :: Hand -> Hand -> Integer -> (Card, Hand)
   removeCard' topPart Empty n = error "removeCard: index exceeds hand size"
-  removeCard' topPart (Add c bottomPart) 0 = (c, topPart `addReverse`
-                                            bottomPart)
-  removeCard' topPart (Add c bottomPart) n = removeCard' (Add c topPart)
-                                            bottomPart (n-1)
+  removeCard' topPart (Add c bottomPart) 0 =
+                        (c, topPart `addReverse` bottomPart)
+  removeCard' topPart (Add c bottomPart) n =
+                        removeCard' (Add c topPart) bottomPart (n-1)
 
   -- Adds one hand on top of the other in reverse order
   addReverse :: Hand -> Hand -> Hand
@@ -148,7 +155,8 @@ module BlackJack where
   -- Check if a card is in a deck before it has been shuffled
   -- then it should be in the deck afterwards
   prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
-  prop_shuffle_sameCards g c h = c `belongsTo` h == c `belongsTo` shuffle g h
+  prop_shuffle_sameCards g c h = c `belongsTo` h ==
+                                c `belongsTo` shuffle g h
 
   -- States that the prize is preserved
   prop_size_shuffle :: StdGen -> Hand -> Bool
