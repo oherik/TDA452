91bb52e5e7a30713e7d3183097c9f042d284263d
Erik Öhrn <erik.ohrn@outlook.com>
2016-11-29 11:02:06 +0100

    Future-proofed add and mul

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,13 +1,25 @@
+data Expr = Num Float
+          | Var Char
+          | Sin Expr
+          | Cos Expr
+          | Add Expr Expr
+          | Mul Expr Expr
+
+
+
+--data Single = Num Integer
+--            | Var Char
+
 
 --TODO should we have cos and sin as expressions?
 ---- B ----
 showExpr :: Expr -> String
 showExpr (Num n) = showNum n
 showExpr (Var v) = [v]    -- om Var sparas som en Char
-showExpr (Cos e) = "cos " ++ showFact e
-showExpr (Sin e) = "sin " ++ showFact e
-showExpr (Add e1 e2) = showAdd e1 e2
-showExpr (Mul e1 e2) = showMul e1 e2
+showExpr (Cos e) = "cos " ++ showArg e
+showExpr (Sin e) = "sin " ++ showArg e
+showExpr (Add e1 e2) = showAdd $ add e1 e2
+showExpr (Mul e1 e2) = showMul $ mul e1 e2
 
 showFact :: Expr -> String
 showFact (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
@@ -18,17 +30,30 @@ showNum :: Float -> String
 showNum n | (n == fromIntegral (round n)) = show $ round n
           | otherwise = show n
 
-showMul :: Expr -> Expr -> String
-showMul (Num 0) _ = showFact (Num 0)
-showMul _ (Num 0) = showFact (Num 0)
-showMul (Num 1) e = showFact e
-showMul e (Num 1) = showFact e
-showMul (Num n1) (Num n2) = showFact (Num (n1+n2))
-showMul (Var x) e = showFact e ++ "*" ++ showFact (Var x)
-showMul e1 e2 = showFact e1 ++ "*" ++ showFact e2
+showAdd :: Expr -> String
+showAdd (Add e1 e2) = showExpr e1 ++ " + " ++ showExpr e2
+showAdd e = showExpr e
 
-showAdd :: Expr -> Expr -> String
-showAdd (Num 0) e = showExpr e
-showAdd e (Num 0) = showExpr e
-showAdd (Num n1) (Num n2) = showExpr (Num (n1+n2))
-showAdd e1 e2 = showExpr e1 ++ " + " ++ showExpr e2
+showMul :: Expr -> String
+showMul (Mul e1 e2) = showFact e1 ++ "*" ++ showFact e2
+showMul e = showExpr e
+
+showArg :: Expr -> String
+showArg (Num n) = showExpr (Num n)
+showArg (Var x) = showExpr (Var x)
+showArg e =  "(" ++ showExpr e ++ ")"
+
+mul :: Expr -> Expr -> Expr
+mul (Num 0) _ = (Num 0)
+mul _ (Num 0) = (Num 0)
+mul (Num 1) e = e
+mul e (Num 1) = e
+mul (Num n1) (Num n2) = (Num (n1*n2))
+mul (Var x) e = (Mul e (Var x))
+mul e1 e2 = (Mul e1 e2)
+
+add :: Expr -> Expr -> Expr
+add (Num 0) e = e
+add e (Num 0) = e
+add (Num n1) (Num n2) = (Num (n1+n2))
+add e1 e2 = (Add e1 e2)
