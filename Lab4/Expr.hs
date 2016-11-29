ed3406eeb40bdb285959eeb60cf9f0965e86ce09
Erik Öhrn <erik.ohrn@outlook.com>
2016-11-29 11:24:11 +0100

    The expression should be complete now, but it's sooo ugly atm

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,25 +1,28 @@
-data Expr = Num Float
-          | Var Char
-          | Sin Expr
-          | Cos Expr
-          | Add Expr Expr
-          | Mul Expr Expr
-
-
-
---data Single = Num Integer
---            | Var Char
-
 
 --TODO should we have cos and sin as expressions?
 ---- B ----
 showExpr :: Expr -> String
 showExpr (Num n) = showNum n
 showExpr (Var v) = [v]    -- om Var sparas som en Char
-showExpr (Cos e) = "cos " ++ showArg e
-showExpr (Sin e) = "sin " ++ showArg e
-showExpr (Add e1 e2) = showAdd $ add e1 e2
-showExpr (Mul e1 e2) = showMul $ mul e1 e2
+showExpr (Cos e) = showCos $ simplified (Cos e)
+showExpr (Sin e) = showSin $ simplified (Sin e)
+showExpr (Add e1 e2) = showAdd $ simplified (Add e1 e2)
+showExpr (Mul e1 e2) = showMul $ simplified (Mul e1 e2)
+
+simplified :: Expr -> Expr
+simplified (Mul e1 e2) = mul (simplified e1) (simplified e2)
+simplified (Add e1 e2) = add (simplified e1) (simplified e2)
+simplified (Sin e) = sin' (simplified e)
+simplified (Cos e) = cos' (simplified e)
+simplified e = e
+
+showCos :: Expr -> String
+showCos (Cos e)= "cos " ++ showArg (simplified e)
+showCos e = showExpr e
+
+showSin :: Expr -> String
+showSin (Sin e)= "sin " ++ showArg (simplified e)
+showSin e = showExpr e
 
 showFact :: Expr -> String
 showFact (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
@@ -57,3 +60,11 @@ add (Num 0) e = e
 add e (Num 0) = e
 add (Num n1) (Num n2) = (Num (n1+n2))
 add e1 e2 = (Add e1 e2)
+
+sin' :: Expr -> Expr
+sin' (Num n) = Num(sin n)
+sin' e = (Sin e)
+
+cos' :: Expr -> Expr
+cos' (Num n) = Num(cos n)
+cos' e = (Cos e)
