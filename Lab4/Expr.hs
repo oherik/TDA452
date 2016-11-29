db40c2c96d3648c2ab5bec4e48784dabb5dab3b3
Erik Öhrn <erik.ohrn@outlook.com>
2016-11-29 10:38:47 +0100

    Smarter function expressions

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,9 +1,11 @@
 
+--TODO should we have cos and sin as expressions?
 ---- B ----
 showExpr :: Expr -> String
 showExpr (Num n) = showNum n
 showExpr (Var v) = [v]    -- om Var sparas som en Char
-showExpr (Fun f e) = f ++ " " ++ showArg e  -- om Fun sparas som en String
+showExpr (Cos e) = "cos " ++ showFact e
+showExpr (Sin e) = "sin " ++ showFact e
 showExpr (Add e1 e2) = showAdd e1 e2
 showExpr (Mul e1 e2) = showMul e1 e2
 
@@ -11,11 +13,7 @@ showFact :: Expr -> String
 showFact (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
 showFact e = showExpr e
 
-showArg :: Expr -> String
-showArg (Num n) = showExpr (Num n) ++ " "
-showArg (Var v) = showExpr (Var v) ++ " "
-showArg e = "(" ++ showExpr e ++ ")"
-
+-- Display numbers without decimal points if they are integers
 showNum :: Float -> String
 showNum n | (n == fromIntegral (round n)) = show $ round n
           | otherwise = show n
@@ -23,11 +21,12 @@ showNum n | (n == fromIntegral (round n)) = show $ round n
 showMul :: Expr -> Expr -> String
 showMul (Num 0) _ = "0"
 showMul _ (Num 0) = "0"
-showMul (Num 1) e = showExpr e
-showMul e (Num 1) = showExpr e
+showMul (Num 1) e = showFact e
+showMul e (Num 1) = showFact e
+showMul (Var x) e = showFact e ++ "*" ++ showFact (Var x)
 showMul e1 e2 = showFact e1 ++ "*" ++ showFact e2
 
 showAdd :: Expr -> Expr -> String
 showAdd (Num 0) e = showExpr e
 showAdd e (Num 0) = showExpr e
-showAdd e1 e2 = showExpr e1 ++ "+" ++ showExpr e2
+showAdd e1 e2 = showExpr e1 ++ " + " ++ showExpr e2
