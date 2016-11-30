b195abf156ab89a74ef2f989996dd15007569423
Erik Öhrn <erik.ohrn@outlook.com>
2016-11-30 16:52:12 +0100

    Should work with the nyew data type

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-
 --TODO should we have cos and sin as expressions?
 ---- B ----
 --TODO test av fancy syntax
@@ -7,32 +5,39 @@ showExpr :: Expr -> String
 showExpr e = showSimplified $ simplified e
     where
       showSimplified :: Expr -> String
-      showSimplified = \case
-        Num n     -> showNum n
-        Var v     -> [v]    -- om Var sparas som en Char
-        Cos e     -> "cos " ++ showArg e
-        Sin e     -> "sin " ++ showArg e
-        Add e1 e2 -> showExpr e1 ++ " + " ++ showExpr e2
-        Mul e1 e2 -> showFact e1 ++ "*" ++ showFact e2
+      showSimplified Num n = showNum n
+      showSimplified Var v = v
+      showSimplified Func f e = showFun f e
+      showSimplified Opr op e1 e2 = showOpr op e1 e2
 
 -- Simplifies an expression (eg (Add (Num 3) (Num 4)) becomes
 -- (Num 7))
 simplified :: Expr -> Expr
-simplified (Mul e1 e2) = mul (simplified e1) (simplified e2)
-simplified (Add e1 e2) = add (simplified e1) (simplified e2)
-simplified (Sin e) = sin' (simplified e)
-simplified (Cos e) = cos' (simplified e)
+simplified (Opr Mul e1 e2) = mul (simplified e1) (simplified e2)
+simplified (Opr Add e1 e2) = add (simplified e1) (simplified e2)
+simplified (Func Sin e) = sin' (simplified e)
+simplified (Func Cos e) = cos' (simplified e)
 simplified e = e
 
 -- Convert a numerical expression to a string
 -- Display numbers without decimal points if they are integers
-showNum :: Float -> String
+showNum :: Double -> String
 showNum n | (n == fromIntegral (round n)) = show $ round n
           | otherwise = show n
 
+-- Convert a function expression to a string
+showFun :: Functions -> Expr -> String
+showFun Cos e = "cos " ++ showArg e
+showFun Sin e = "sin " ++ showArg e
+
+-- Convert an operation expression to a string
+showOpr :: Operators -> Expr -> Expr -> String
+showOpr Mul e1 e2 = showFact e1 ++ "*" ++ showFact e2
+showOpr Add e1 e2 = showExpr e1 ++ " + " ++ showExpr e2
+
 -- A factor that's an addition should have parentheses around it
 showFact :: Expr -> String
-showFact (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
+showFact (Opr Add e1 e2) = "(" ++ showExpr (Opr Add e1 e2) ++ ")"
 showFact e = showExpr e
 
 -- Arguments consisting of other functions or additions should have
@@ -49,22 +54,22 @@ mul _ (Num 0)   = Num 0
 mul (Num 1) e   = e
 mul e (Num 1)   = e
 mul (Num n1) (Num n2) = Num (n1*n2)
-mul (Var x) e   = Mul e (Var x)
-mul e1 e2       = Mul e1 e2
+mul (Var x) e   = Opr Mul e (Var x)
+mul e1 e2       = Opr Mul e1 e2
 
 -- Simplifies an addition
 add :: Expr -> Expr -> Expr
 add (Num 0) e   = e
 add e (Num 0)   = e
 add (Num n1) (Num n2) = Num (n1+n2)
-add e1 e2       = Add e1 e2
+add e1 e2       = Opr Add e1 e2
 
 -- Simplifies a sin expression
 sin' :: Expr -> Expr
 sin' (Num n)    = Num(sin n)
-sin' e          = Sin e
+sin' e          = Func Sin e
 
 -- Simplifies a cos expression
 cos' :: Expr -> Expr
 cos' (Num n)    = Num(cos n)
-cos' e          = Cos e
+cos' e          = Func Cos e
