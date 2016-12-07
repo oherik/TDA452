c54b8706c4a9cd3ef3fc47c19d74e27389f77b75
Erik Öhrn <erik.ohrn@outlook.com>
2016-12-07 11:25:02 +0100

    Added out-commented alternative porperty for show/read expr

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -149,6 +149,17 @@ prop_ShowReadExpr :: Expr -> Bool
 prop_ShowReadExpr ex = showExpr ex ==
   (showExpr (fromJust ((readExpr (showExpr ex)))))
 
+-- TODO: or like this?
+-- prop_ShowReadExpr :: Expr -> Property
+ --prop_ShowReadExpr ex = (fromJust $ readExpr (showExpr ex)) == ex		 +prop_ShowReadExpr ex = forAll rNum (\ x -> eval (fromJust (readExpr (showExpr ex))) x `almostEqual` eval ex x)
+ --		  where
+ --   almostEqual :: Double -> Double -> Bool
+ --   almostEqual x y = abs (x - y) < 0.001
+ --
+ --rNum :: Gen Double
+ --rNum = do n <- arbitrary
+ --          return n
+
 -- Only Num and Var count towards the length (excluding the remainder)
 arbExpr :: Int -> Gen Expr
 arbExpr size = frequency [(4,rNum),
